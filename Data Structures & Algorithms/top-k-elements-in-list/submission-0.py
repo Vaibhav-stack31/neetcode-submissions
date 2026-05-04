e7ba02c55f5a322a264229aac33d90b0ff173b3d
Vaibhav Tiwari <tiwariv1669@gmail.com>
2026-05-04 23:19:05 +0530

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
@@ -0,0 +1,6 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        frequency = {}
+        for num in nums:
+            frequency[num] = frequency.get(num, 0) + 1
+        return (heapq.nlargest(k, frequency.keys(), key=frequency.get))
